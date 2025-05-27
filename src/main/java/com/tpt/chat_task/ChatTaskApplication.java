9c4b9885816f49daa1cbb95dd955d7b4b6c50375
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2025-05-27 20:26:19 +0700

    init project

---
+++ src/main/java/com/tpt/chat_task/ChatTaskApplication.java
@@ -0,0 +1,13 @@
+package com.tpt.chat_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpt/chat_task/ChatTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpt.chat_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
