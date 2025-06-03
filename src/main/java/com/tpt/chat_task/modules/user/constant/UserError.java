acb9447c8a8e71c48d074f77720efd6b918a1149
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2025-06-03 22:12:30 +0700

    [feat-profile] add profile service

---
+++ src/main/java/com/tpt/chat_task/modules/user/constant/UserError.java
@@ -0,0 +1,7 @@
+package com.tpt.chat_task.modules.user.constant;
+
+public class UserError {
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String CURRENT_PASSWORD_WRONG = "Current password wrong";
+    public static final String PASSWORD_MISMATCH = "Password mismatch";
+}
---
+++ src/main/java/com/tpt/chat_task/modules/user/controller/UserController.java
@@ -0,0 +1,74 @@
+package com.tpt.chat_task.modules.user.controller;
+
+import com.tpt.chat_task.common.constant.AppConstant;
+import com.tpt.chat_task.common.constant.JwtConstant;
+import com.tpt.chat_task.common.dto.SuccessResponse;
+import com.tpt.chat_task.modules.user.dto.request.ChangePasswordRequest;
+import com.tpt.chat_task.modules.user.dto.request.UpdateProfileRequest;
+import com.tpt.chat_task.modules.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getProfile(
+            @RequestHeader(JwtConstant.JWT_HEADER) String bearerToken
+    ) {
+        String accessToken = bearerToken.substring(7);
+        SuccessResponse successResponse = SuccessResponse.builder()
+                .data(this.userService.getProfile(accessToken))
+                .build();
+        return ResponseEntity.ok(successResponse);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getListUsers(
+            @RequestParam(name = "page", required = false, defaultValue = AppConstant.PAGE) Integer page,
+            @RequestParam(name = "paging", required = false, defaultValue = AppConstant.PAGING) Integer paging
+    ) {
+        return ResponseEntity.ok(this.userService.getListUsers(page, paging));
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUserById(
+            @PathVariable String id
+    ) {
+        SuccessResponse successResponse = SuccessResponse.builder()
+                .data(this.userService.getUserById(id))
+                .build();
+        return ResponseEntity.ok(successResponse);
+    }
+
+    @PatchMapping("/profile")
+    public ResponseEntity<?> updateProfile(
+            @RequestBody @Valid UpdateProfileRequest request,
+            @RequestHeader(JwtConstant.JWT_HEADER) String bearerToken
+    ) {
+        String accessToken = bearerToken.substring(7);
+        SuccessResponse successResponse = SuccessResponse.builder()
+                .data(this.userService.updateProfile(accessToken, request))
+                .build();
+        return ResponseEntity.ok(successResponse);
+    }
+
+    @PatchMapping("/profile/change-password")
+    public ResponseEntity<?> changePassword(
+            @RequestBody @Valid ChangePasswordRequest request,
+            @RequestHeader(JwtConstant.JWT_HEADER) String bearerToken
+    ) throws BadRequestException {
+        String accessToken = bearerToken.substring(7);
+        SuccessResponse successResponse = SuccessResponse.builder()
+                .data(this.userService.changePassword(accessToken, request))
+                .build();
+        return ResponseEntity.ok(successResponse);
+    }
+}
