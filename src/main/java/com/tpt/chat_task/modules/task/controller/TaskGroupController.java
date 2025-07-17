d49883cbd5a8ceeb0e350c919d276e6bad086118
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2025-07-17 23:57:13 +0700

    add task group controller

--- src/main/java/com/tpt/chat_task/modules/task/controller/TaskGroupController.java
+++ src/main/java/com/tpt/chat_task/modules/task/controller/TaskGroupController.java
@@ -2,10 +2,12 @@ package com.tpt.chat_task.modules.task.controller;
 
 import com.tpt.chat_task.common.dto.SuccessResponse;
 import com.tpt.chat_task.modules.task.dto.request.CreateTaskGroupRequest;
+import com.tpt.chat_task.modules.task.dto.request.UpdateTaskGroupRequest;
 import com.tpt.chat_task.modules.task.entity.TaskGroup;
 import com.tpt.chat_task.modules.task.service.TaskGroupService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,5 +27,51 @@ public class TaskGroupController {
                 .data(taskGroupService.addNewTaskGroup(boardId, request))
                 .build();
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{boardId}/group-tasks/{groupTaskId}")
+    public ResponseEntity<?> updateTaskGroup(
+            @PathVariable String boardId,
+            @PathVariable String groupTaskId,
+            @RequestBody UpdateTaskGroupRequest request
+    ) {
+        SuccessResponse response = SuccessResponse.builder()
+                .data(taskGroupService.updateTaskGroup(boardId, groupTaskId, request))
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{boardId}/group-tasks/{groupTaskId}")
+    public ResponseEntity<?> getTaskGroupDetail(
+            @PathVariable String boardId,
+            @PathVariable String groupTaskId
+    ) {
+        SuccessResponse response = SuccessResponse.builder()
+                .data(taskGroupService.getTaskGroupDetail(boardId, groupTaskId))
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{boardId}/group-tasks/{groupTaskId}")
+    public ResponseEntity<?> updateTaskGroup(
+            @PathVariable String boardId,
+            @PathVariable String groupTaskId
+    ) {
+        SuccessResponse response = SuccessResponse.builder()
+                .data(taskGroupService.deleteTaskGroup(boardId, groupTaskId))
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{boardId}/group-tasks/{groupTaskId}/change-position/{newPosition}")
+    public ResponseEntity<?> updateTaskGroup(
+            @PathVariable String boardId,
+            @PathVariable String groupTaskId,
+            @PathVariable int newPosition
+    ) throws BadRequestException {
+        SuccessResponse response = SuccessResponse.builder()
+                .data(taskGroupService.changePosition(boardId, groupTaskId, newPosition))
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
